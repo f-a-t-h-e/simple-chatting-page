74886beea18391b9fdee758b106bd5e73e5c3a57
f-a-t-h-e <77131433+f-a-t-h-e@users.noreply.github.com>
2023-03-13 21:08:30 +0200

    Add gateways using a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,53 @@
+import "dotenv/config";
 import express from "express";
+import jwt from "jsonwebtoken";
 import * as http from "http";
-import * as WebSocket from "ws";
+import cookieParser from "cookie-parser";
+
+import config, { keys } from "./utils/config";
+
 import path from "path";
+
+import connectDB from "./database/connect";
+import router from "./routes";
+import ErrorHandler from "./middlewares/ErrorHandler";
+import { createWS } from "./gateway";
+import User from "./models/User";
 
 const app = express();
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+app.use(cookieParser());
 
-wss.on("connection", (ws: WebSocket, req) => {
-  ws.on("message", (data: string, isBinary) => {
-    const message = JSON.parse(data);
-    console.log("Message from client :: ", message);
+createWS(server);
 
-    wss.clients.forEach((client) => {
-      if (/*client !== ws && */ client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ ...message, myMessage: client === ws }));
-      }
-    });
-  });
-  // ws.send(JSON.stringify({ message: "Welcome to chat !!" }));
+app.get("/", async (req, res) => {
+  console.log();
+
+  const payload = jwt.verify(
+    req.cookies["authorize-chat"] || "",
+    keys.JWT_SECRET_KEY
+  ) as any;
+  const user = await User.findById(payload.id);
+
+  res.sendFile(
+    path.join(__dirname, "..", "public", user ? "index.html" : "auth.html")
+  );
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
+app.use(express.json());
 
-server.listen(process.env.PORT || 8080, () => {
-  console.log("Server started");
+app.use("/api/v1", router);
+
+app.use(ErrorHandler);
+
+server.listen(process.env.PORT || 3000, async () => {
+  try {
+    config();
+
+    await connectDB();
+    console.log("Server started");
+  } catch (error) {
+    console.log("🚀 ~ file: index.ts:35 ~ server.listen ~ error:", error);
+  }
 });
